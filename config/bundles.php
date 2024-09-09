c2b4a12b62f877281ede4997ca188e4fd9462592
Jesper Kristensen <cableman@linuxdev.dk>
2024-09-09 14:08:27 +0200

    2309: Added metrics bundel

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    ItkDev\MetricsBundle\ItkDevMetricsBundle::class => ['all' => true],
 ];
