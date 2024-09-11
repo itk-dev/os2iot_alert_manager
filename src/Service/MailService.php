c9c8aba4907107d02a81edd3c3415852c0d7c718
Jesper Kristensen <cableman@linuxdev.dk>
2024-09-11 16:03:01 +0200

    2305: Added test templates for mails

--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use App\Exception\MailException;
 use ItkDev\MetricsBundle\Service\MetricsService;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
@@ -20,31 +21,34 @@ final readonly class MailService
     }
 
     /**
-     * Sends an email and tracks metrics on success or failure.
+     * Send an email.
      *
      * @param string $to
-     *   Recipient email address
+     *   The recipient's email address
+     * @param array $context
+     *   The context for the email template
      * @param string $subject
-     *   Subject of the email
-     * @param string $htmlMessage
-     *   HTML content of the email
-     * @param string $textMsg
-     *   Plain text content of the email
+     *   The subject of the email. Defaults to 'Test mail from alter manager'.
+     * @param string $htmlTemplate
+     *   The HTML template for the email. Defaults to 'test.html.twig'.
+     * @param string $textTemplate
+     *   The text template for the email. Defaults to 'test.txt.twig'.
      *
      * @throws MailException
-     *   If the email cannot be sent
      */
-    public function sendEmail(string $to, string $subject, string $htmlMessage, string $textMsg): void
+    public function sendEmail(string $to, array $context, string $subject = 'Test mail from alter manager', string $htmlTemplate = 'test.html.twig', string $textTemplate = 'test.txt.twig'): void
     {
-        $email = (new Email())
+        $email = (new TemplatedEmail())
             ->from($this->fromAddress)
             ->to($to)
             ->cc($this->defaultAddress)
             ->replyTo($this->replyAddress)
             ->priority(Email::PRIORITY_HIGH)
             ->subject($subject)
-            ->text($textMsg)
-            ->html($htmlMessage);
+            ->textTemplate('mails/'.$textTemplate)
+            ->htmlTemplate('mails/'.$htmlTemplate)
+            ->context($context)
+        ;
 
         try {
             $this->mailer->send($email);
